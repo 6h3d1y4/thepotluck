daab941a23c435b2d9227eced740eb6bcc48cc47
oefosa <o.efosa-agho@rgu.ac.uk>
2022-03-15 14:25:45 +0000

    login page

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+
+$servername = 'localhost';
+$database_name = 'the potluck';
+$username = "root";
+$password = "";
---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<!-- //Connect to a database and retreive value from  -->
+<!-- //Establishing connection with our database -->
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>THIS IS HOME PAGE</h1>
+    <form method="post" action="">
+        <p>Email: </p>
+        <input type="email" name="email"> 
+        <p>Password: </p> 
+        <input type="password" name="password">
+        <input type="submit" name="login">
+    </form>
+
+</body>
+</html>
